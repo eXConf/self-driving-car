ad4899ce859fff671166a8b3a74acf79ea5efae4
eXConf <sergey.sosedkin@gmail.com>
2022-05-24 20:33:05 +0300

    + save/load best brain

--- public/js/main.js
+++ public/js/main.js
@@ -7,13 +7,27 @@ const networkCanvas = document.getElementById('networkCanvas');
 networkCanvas.width = 300;
 const carCtx = carCanvas.getContext('2d');
 const networkCtx = networkCanvas.getContext('2d');
+renderButtons();
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+const lsBestBrain = localStorage.getItem('bestBrain');
+if (lsBestBrain) {
+    bestCar.brain = JSON.parse(lsBestBrain);
+}
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY', 2),
 ];
 animate();
+function save() {
+    localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+function discard() {
+    localStorage.removeItem('bestBrain');
+}
 function generateCars(N) {
     const cars = [];
     for (let i = 0; i < N; i++) {
@@ -28,10 +42,7 @@ function animate() {
     for (let i = 0; i < cars.length; i++) {
         cars[i].update(road.borders, traffic);
     }
-    const bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)));
-    if (!bestCar) {
-        throw new Error("No best car!");
-    }
+    bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)));
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
     carCtx.save();
@@ -51,4 +62,17 @@ function animate() {
         Visualizer.drawNetwork(networkCtx, bestCar.brain);
     }
     requestAnimationFrame(animate);
+}
+function renderButtons() {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'verticalButtons');
+    const saveBtn = document.createElement('button');
+    saveBtn.innerText = '💾';
+    saveBtn.onclick = save;
+    const discardBtn = document.createElement('button');
+    discardBtn.innerText = '🗑';
+    discardBtn.onclick = discard;
+    div.appendChild(saveBtn);
+    div.appendChild(discardBtn);
+    carCanvas.after(div);
 }
--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,34 @@ networkCanvas.width = 300;
 const carCtx = carCanvas.getContext('2d') as CanvasRenderingContext2D;
 const networkCtx = networkCanvas.getContext('2d') as CanvasRenderingContext2D;
 
+renderButtons();
+
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+
+const lsBestBrain = localStorage.getItem('bestBrain');
+if (lsBestBrain) {
+  bestCar.brain = JSON.parse(lsBestBrain);
+}
 
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY', 2),
 ]
 
 animate();
+
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem('bestBrain');
+}
 
 function generateCars(N: number) {
   const cars = [];
@@ -40,11 +58,7 @@ function animate() {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)));
-
-  if (!bestCar) {
-    throw new Error("No best car!");
-  }
+  bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)))!;
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
@@ -71,4 +85,22 @@ function animate() {
     Visualizer.drawNetwork(networkCtx, bestCar.brain);
   }
   requestAnimationFrame(animate);
+}
+
+function renderButtons() {
+  const div = document.createElement('div');
+  div.setAttribute('id','verticalButtons');
+
+  const saveBtn = document.createElement('button');
+  saveBtn.innerText = '💾';
+  saveBtn.onclick = save;
+
+  const discardBtn = document.createElement('button');
+  discardBtn.innerText = '🗑';
+  discardBtn.onclick = discard;
+
+  div.appendChild(saveBtn);
+  div.appendChild(discardBtn);
+
+  carCanvas.after(div);
 }
